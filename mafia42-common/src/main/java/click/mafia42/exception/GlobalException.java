0351e1f2f4c39ba92f51815333f8f73bbd8a8d07
이현준 <11dlguswns@naver.com>
2025-09-09 11:10:06 +0900

    feat :: 예외 추가

--- mafia42-common/src/main/java/click/mafia42/exception/GlobalException.java
+++ mafia42-common/src/main/java/click/mafia42/exception/GlobalException.java
@@ -4,7 +4,13 @@ public class GlobalException extends RuntimeException {
     public GlobalException(GlobalExceptionCode code) {
         super("[ " + code.name() + " ] : " + code.getMessage());
     }
+    public GlobalException(GlobalExceptionCode code, Exception e) {
+        super("[ " + code.name() + " ] : " + code.getMessage(), e);
+    }
     public GlobalException(GlobalExceptionCode code, String detailMassage) {
         super("[ " + code.name() + " ] : " + code.getMessage() + " | detailMassage : " + detailMassage);
+    }
+    public GlobalException(GlobalExceptionCode code, String detailMassage, Exception e) {
+        super("[ " + code.name() + " ] : " + code.getMessage() + " | detailMassage : " + detailMassage, e);
     }
 }
--- mafia42-common/src/main/java/click/mafia42/exception/GlobalExceptionCode.java
+++ mafia42-common/src/main/java/click/mafia42/exception/GlobalExceptionCode.java
@@ -5,8 +5,19 @@ public enum GlobalExceptionCode {
     UNSUPPORTED_COMMAND("처리할 수 없는 커멘드입니다."),
 
     MALFORMED_JSON("잘못된 JSON 형식입니다."),
-    INVALID_REQUEST("잘못된 요청입니다.")
-    ;
+    INVALID_REQUEST("잘못된 요청입니다."),
+    MALFORMED_TOKEN("잘못된 JWT 형식입니다."),
+
+    DB_CONNECTION_FAIL("데이터베이스 연결에 실패했습니다."),
+    DB_DISCONNECTION_FAIL("데이터베이스 정상 종료에 실패했습니다."),
+    NOT_FOUND_ENV("필수 환경변수 값이 존재하지 않습니다."),
+    TRANSACTION_FAIL("트랜잭션 작업 도중 오류가 발생했습니다."),
+
+    PASSWORD_MISMATCH("비밀번호가 일치하지 않습니다."),
+    NOT_FOUND_REFRESH_TOKEN("리프레쉬 토큰이 존재하지 않습니다."),
+    NOT_FOUND_USER("유저가 존재하지 않습니다."),
+    USER_ALREADY_EXISTS("이미 존재하는 유저입니다."),
+    SYNC_PROCESS_EXCEPTION("동기 작업 도중 오류가 발생했습니다.");
 
     private final String message;
 
