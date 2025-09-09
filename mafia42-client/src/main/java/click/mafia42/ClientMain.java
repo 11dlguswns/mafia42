51b6289ee9b3a7241aa807340694a36b6000360d
이현준 <11dlguswns@naver.com>
2025-09-09 11:16:58 +0900

    feat :: ClassLoader 설정 추가

--- mafia42-client/src/main/java/click/mafia42/ClientMain.java
+++ mafia42-client/src/main/java/click/mafia42/ClientMain.java
@@ -2,6 +2,8 @@ package click.mafia42;
 
 public class ClientMain {
     public static void main(String[] args) throws Exception {
+        Class.forName("org.hibernate.validator.internal.util.Version");
+        Class.forName("org.hibernate.validator.internal.engine.ValidatorImpl");
         Mafia42Client mafia42Client = new Mafia42Client();
         mafia42Client.start();
     }
--- mafia42-server/src/main/java/click/mafia42/ServerMain.java
+++ mafia42-server/src/main/java/click/mafia42/ServerMain.java
@@ -4,6 +4,11 @@ import click.mafia42.database.ChannelManager;
 
 public class ServerMain {
     public static void main(String[] args) throws Exception {
+        Class.forName("click.mafia42.database.properties.DBProperties");
+        Class.forName("click.mafia42.security.properties.JwtProperties");
+        Class.forName("click.mafia42.database.transaction.TransactionManager");
+        Class.forName("org.hibernate.validator.internal.util.Version");
+        Class.forName("org.hibernate.validator.internal.engine.ValidatorImpl");
         ChannelManager channelManager = new ChannelManager();
         Mafia42Server mafia42Server = new Mafia42Server(channelManager);
         mafia42Server.start();
