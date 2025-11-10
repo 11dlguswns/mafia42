db77f41449c239077d130cfb4cefd0739e201ba0
이현준 <11dlguswns@naver.com>
2025-11-10 13:39:26 +0900

    feat :: 상태 체크용 포트 번호 추가

--- mafia42-server/src/main/java/click/mafia42/Mafia42Server.java
+++ mafia42-server/src/main/java/click/mafia42/Mafia42Server.java
@@ -4,8 +4,10 @@ import click.mafia42.database.GameRoomManager;
 import click.mafia42.initializer.ServerSocketChannelInitializer;
 import click.mafia42.database.ChannelManager;
 import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.Unpooled;
 import io.netty.channel.*;
 import io.netty.channel.nio.NioIoHandler;
+import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 
 public class Mafia42Server {
@@ -22,20 +24,47 @@ public class Mafia42Server {
                 EventLoopGroup bossGroup = new MultiThreadIoEventLoopGroup(1, NioIoHandler.newFactory());
                 EventLoopGroup workerGroup = new MultiThreadIoEventLoopGroup(Runtime.getRuntime().availableProcessors() * 2, NioIoHandler.newFactory())
         ) {
-            ServerBootstrap bootstrap = new ServerBootstrap();
-            bootstrap.group(bossGroup, workerGroup)
-                    .channel(NioServerSocketChannel.class)
-                    .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 3000)
-                    .option(ChannelOption.SO_BACKLOG, 1024)
-                    .childOption(ChannelOption.TCP_NODELAY, true)
-                    .childOption(ChannelOption.SO_LINGER, 0)
-                    .childOption(ChannelOption.SO_KEEPALIVE, true)
-                    .childOption(ChannelOption.SO_REUSEADDR, true)
-                    .childHandler(new ServerSocketChannelInitializer(channelManager, gameRoomManager));
+            createHealthCheckBootstrap(bossGroup, workerGroup).bind(8081).sync();
 
-            ChannelFuture channelFuture = bootstrap.bind(8080).sync();
-            channelFuture.channel().closeFuture().sync();
+            ServerBootstrap mafia42Bootstrap = createMafia42Bootstrap(bossGroup, workerGroup);
+            ChannelFuture mafia42ChannelFuture = mafia42Bootstrap.bind(8080).sync();
+
+            mafia42ChannelFuture.channel().closeFuture().sync();
         }
+    }
+
+    private ServerBootstrap createMafia42Bootstrap(EventLoopGroup bossGroup, EventLoopGroup workerGroup) {
+        ServerBootstrap mafia42Bootstrap = new ServerBootstrap();
+        mafia42Bootstrap.group(bossGroup, workerGroup)
+                .channel(NioServerSocketChannel.class)
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 3000)
+                .option(ChannelOption.SO_BACKLOG, 1024)
+                .childOption(ChannelOption.TCP_NODELAY, true)
+                .childOption(ChannelOption.SO_LINGER, 0)
+                .childOption(ChannelOption.SO_KEEPALIVE, true)
+                .childOption(ChannelOption.SO_REUSEADDR, true)
+                .childHandler(new ServerSocketChannelInitializer(channelManager, gameRoomManager));
+
+        return mafia42Bootstrap;
+    }
+
+    private ServerBootstrap createHealthCheckBootstrap(EventLoopGroup bossGroup, EventLoopGroup workerGroup) {
+        ServerBootstrap healthCheckBootstrap = new ServerBootstrap();
+        healthCheckBootstrap.group(bossGroup, workerGroup)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel ch) {
+                        ch.pipeline().addLast(new SimpleChannelInboundHandler<>() {
+                            @Override
+                            protected void channelRead0(ChannelHandlerContext ctx, Object msg) {
+                                ctx.writeAndFlush(Unpooled.EMPTY_BUFFER).addListener(ChannelFutureListener.CLOSE);
+                            }
+                        });
+                    }
+                });
+
+        return healthCheckBootstrap;
     }
 
 }
